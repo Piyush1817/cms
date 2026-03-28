55d72cecac5ed7f98d31ebf09ece4d04fe1a0505
Piyush1817 <lakwanisuresh086@gmail.com>
2026-03-28 12:15:27 +0530

    added controller, and config for postman checking

---
+++ src/main/java/com/project/cms/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.project.cms.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.cms.entity.User;
+import com.project.cms.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+}
